3bcc5034afdc0f37906a95fe10816be2adbb6534
Casey Wittrock <caseywittrockmn@gmail.com>
2023-05-24 22:51:50 -0500

    added ingredients to seed file

--- seed.py
+++ seed.py
@@ -1,22 +1,40 @@
 from app import app
-from models import db, Cupcake
+from models import db, Cupcake, Ingredient, Recipe
 
-
-db.drop_all()
-db.create_all()
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+ing1 = Ingredient(ingredient_name="cherries")
+ing2 = Ingredient(ingredient_name="chocolate")
+ing3 = Ingredient(ingredient_name="vanilla")
+ing4 = Ingredient(ingredient_name="strawberries")
+ing5 = Ingredient(ingredient_name="flour")
+ing6 = Ingredient(ingredient_name="sugar")
 
 c1 = Cupcake(
     flavor="cherry",
-    size="large",
+    size="big",
     rating=5,
+    ingredients=[ing1, ing2],
 )
 
 c2 = Cupcake(
     flavor="chocolate",
-    size="small",
+    size="mini",
     rating=9,
-    image="https://www.bakedbyrachel.com/wp-content/uploads/2018/01/chocolatecupcakesccfrosting1_bakedbyrachel.jpg"
+    ingredients=[ing1, ing2],
+    image="https://www.bakedbyrachel.com/wp-content/uploads/2018/01/chocolatecupcakesccfrosting1_bakedbyrachel.jpg",
 )
 
-db.session.add_all([c1, c2])
-db.session.commit()
+c2 = Cupcake(
+    flavor="vanilla",
+    size="regular",
+    rating=4,
+    ingredients=[ing3, ing4],
+    image="https://www.bakedbyrachel.com/wp-content/uploads/2018/01/vanillacupcakesccfrosting1_bakedbyrachel.jpg",
+)
+
+
+with app.app_context():
+    db.session.add_all([c1, c2, ing1, ing2, ing3, ing4, ing5, ing6])
+    db.session.commit()
